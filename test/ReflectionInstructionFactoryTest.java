85c778c1bc33a5c16996b9626e548f9027b35209
FabianZ1990 <fabian.zischler@gmail.com>
2023-03-03 12:21:38 +0000

    wrote a test for the out instruction. Even with 3 inputs, the Factory correctly recognizes that the Constructor of OutInstruction only takes 2 parameters. Method creates the right instruction

--- test/ReflectionInstructionFactoryTest.java
+++ test/ReflectionInstructionFactoryTest.java
@@ -86,5 +86,23 @@ class ReflectionInstructionFactoryTest {
         instruction.execute(machine);
         Assertions.assertEquals(1, machine.getRegisters().get(EAX));
     }
+    @Test
+    void executeValid4() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        String testOpcode = "out";
+        ArrayList<String> testInput = new ArrayList<>(Arrays.asList("f2", "EAX", "1"));
+        ReflectionInstructionFactory testFac = ReflectionInstructionFactory.getInstance();
+        Instruction instruction;
+        try {
+            instruction = testFac.createInstruction(testOpcode, testInput);
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+                 OpcodeNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        instruction.execute(machine);
+        Assertions.assertEquals("The current value stored in register EAX is 5",
+                "The current value stored in register " + EAX.name() + " is " + machine.getRegisters().get(EAX));
+    }
 
 }
